0b74950d585bc90b6320d54930ffc2e9e254498d
Happiness Balogun <happinessblgn0@gmail.com>
2019-12-16 12:33:55 +0100

    New updates

---
+++ app.js
@@ -0,0 +1,24 @@
+AOS.init({
+  duration: 1200
+});
+
+var SCROLLING_NAVBAR_OFFSET_TOP = 50;
+console.log("SCROLLING_NAVBAR_OFFSET_TOP");
+$(window).on("scroll", function() {
+  var $navbar = $(".navbar");
+
+  if ($navbar.length) {
+    if ($navbar.offset().top > SCROLLING_NAVBAR_OFFSET_TOP) {
+      $(".scrolling-navbar").addClass("top-nav-collapse");
+    } else {
+      $(".scrolling-navbar").removeClass("top-nav-collapse");
+    }
+  }
+});
+
+function heroClassToggle() {
+  var element = document.getElementById("hero-section");
+  element.classList.toggle("mystyle");
+}
+
+
